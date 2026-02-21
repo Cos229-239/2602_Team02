1330e4652efdfb5254837bb57bc706ec5fa36333
Lesly-AM <LDMorales1@student.fullsail.edu>
2026-02-20 19:32:41 -0500

    Structural layout complete for create event activity page.
    Structural layout complete for create event activity page - includes event details page.

--- WePartyApp/app/src/main/java/com/example/wepartyapp/ui/create_event/CreateEventActivity.kt
+++ WePartyApp/app/src/main/java/com/example/wepartyapp/ui/create_event/CreateEventActivity.kt
@@ -6,12 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Create
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -19,6 +27,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -45,25 +56,54 @@ fun CreateEventScreenUI() {
     ) {
         Column(
             modifier = Modifier
-                .fillMaxWidth()
+                .fillMaxSize()
                 .padding(16.dp)
         ) {
-            IconButton(onClick = {})                                        //back to home pg btn
-            {
-                Icon(
-                    imageVector = Icons.Default.ArrowBack,
-                    contentDescription = "Home"
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(onClick = {/*EXECUTABLE CODE*/}) {                        //back to home pg btn
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = null,
+                        Modifier.size(35.dp)
+                    )
+                }
+                Text(
+                    text = "Home",
+                    fontSize = 20.sp
                 )
             }
-            Icon(                                                           //pg icon
-                imageVector = Icons.Default.Create,
-                contentDescription = null
-            )
-            Text(                                                           //pg title
-                text = "Create Event (Coming Soon)",
-                color = Color.Black,
-                fontSize = 18.sp
-            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Icon(                                                           //pg icon
+                    imageVector = Icons.Default.Create,
+                    contentDescription = null,
+                    Modifier.size(60.dp),
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(                                                           //pg title
+                    text = "Create Event",
+                    color = Color.Black,
+                    fontSize = 30.sp,
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            EventDetailsScreenUI()
+        }
+        Button(
+            onClick = {/*EXECUTABLE CODE*/},
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp),
+        ) {
+            Text(text = "Next: Add Items")
         }
     }
 }
