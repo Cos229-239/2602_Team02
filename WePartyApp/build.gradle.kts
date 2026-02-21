eab5f3e93ee93f69dde3143a7fbfd447d0628690
Rodford <38554591+Rodford@users.noreply.github.com>
2026-02-21 00:42:36 -0500

    Revert "Add password recovery flow with validation and navigation"

--- WePartyApp/build.gradle.kts
+++ WePartyApp/build.gradle.kts
@@ -2,9 +2,5 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
-<<<<<<< Updated upstream
     alias(libs.plugins.google.services) apply false
 }
-=======
-}
->>>>>>> Stashed changes
