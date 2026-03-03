241763aa9af85bdce302f1b84cf46083dd13e2dc
Lesly-AM <LDMorales1@student.fullsail.edu>
2026-03-02 21:09:46 -0500

    Updating Main activity
    Updating Main activity from the dev I just pulled

--- WePartyApp/app/src/main/java/com/example/wepartyapp/ui/home/MainActivity.kt
+++ WePartyApp/app/src/main/java/com/example/wepartyapp/ui/home/MainActivity.kt
@@ -124,10 +124,8 @@ fun MainScreen(initialTab: Int = 0) { // <-- ADDED THIS: Accept the initialTab p
                 0 -> HomeScreenUI(viewModel = eventViewModel, onNotificationsClick = { selectedTab = 8 })
                 1 -> CalendarScreenUI(viewModel = eventViewModel) // <-- Passed the ViewModel to fix the error!
                 // 2 -> Create Event Activity Launched In Navigation Bar
-                3 -> ConsolidatedShoppingListScreenUI()
-                4 -> EventInboxScreen(viewModel = eventViewModel)
                 3 -> ConsolidatedShoppingListScreenUI(viewModel = eventViewModel)   //LM
-//                4 -> EventsUI()
+                4 -> EventInboxScreen(viewModel = eventViewModel)
                 5 -> DietaryPreferencesScreenUI( onBack = { selectedTab = 6 } )
                 6 -> ProfileScreenUI(
                     onEditDietaryClick = { selectedTab = 5 },
